80647f06db976552646a9808d0e849a0397789ec
Ulvah Awaliyah <ulvahboes@gmail.com>
2022-12-22 07:40:47 -0300

    first commit

---
+++ src/test/java/com/awaliyah/ulvah/movie/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awaliyah.ulvah.movie.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
